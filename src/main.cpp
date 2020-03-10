4fc05186fba9a14eccf528331f4d13adc3c17c00
Usman Mehmood <usmanmehmood55@gmail.com>
2020-03-10 10:20:25 +0500

    AutoPID inclusion

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,64 @@
 #include <Arduino.h>
+#include <AutoPID.h>
 
-void setup() 
-{
+//#include <OneWire.h>
 
+//pins
+#define POT_PIN 35
+#define OUTPUT_PIN 36
+#define TEMP_PROBE_PIN 32
+#define LED_PIN 6
+
+#define TEMP_READ_DELAY 800 //can only read digital temp sensor every ~750ms
+
+//pid settings and gains
+#define OUTPUT_MIN 0
+#define OUTPUT_MAX 255
+#define KP .12
+#define KI .0003
+#define KD 0
+
+double temperature, setPoint, outputVal;
+
+//input/output variables passed by reference, so they are updated automatically
+AutoPID myPID(&temperature, &setPoint, &outputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
+
+unsigned long lastTempUpdate; //tracks clock time of last temp update
+
+//call repeatedly in loop, only updates after a certain time interval
+//returns true if update happened
+bool updateTemperature() {
+  if ((millis() - lastTempUpdate) > TEMP_READ_DELAY) {
+    temperature = analogRead(TEMP_PROBE_PIN); //get temp reading
+    lastTempUpdate = millis();
+    return true;
+  }
+  return false;
 }
+
+
+void setup() {
+  pinMode(POT_PIN, INPUT);
+  pinMode(OUTPUT_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  temperatureSensors.begin();
+  temperatureSensors.requestTemperatures();
+  while (!updateTemperature()) {} //wait until temp sensor updated
+
+  //if temperature is more than 4 degrees below or above setpoint, OUTPUT will be set to min or max respectively
+  myPID.setBangBang(4);
+  //set PID update interval to 4000ms
+  myPID.setTimeStep(4000);
+
+}//void setup
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  updateTemperature();
+  setPoint = analogRead(POT_PIN);
+  myPID.run(); //call every loop, updates automatically at certain time interval
+  analogWrite(OUTPUT_PIN, outputVal);
+  digitalWrite(LED_PIN, myPID.atSetPoint(1)); //light up LED when we're at setpoint +-1 degree
+
+}//void loop
